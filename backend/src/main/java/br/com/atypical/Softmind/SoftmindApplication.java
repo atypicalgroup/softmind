2c9d83416fe7fa001f26d7ceef0065a2d69b412a
Misael Marcelino <misael.marcelino@outlook.com>
2025-08-21 19:37:33 -0300

    Inciando desenvolvimento backend GS 2 2025

---
+++ backend/src/main/java/br/com/atypical/Softmind/SoftmindApplication.java
@@ -0,0 +1,13 @@
+package br.com.atypical.Softmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmindApplication.class, args);
+	}
+
+}
