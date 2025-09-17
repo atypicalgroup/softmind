144b726eae10c14eb01a5ffb09a7d825f618b72d
Edilberto Morais <edilbertocmorais@gmail.com>
2025-09-17 20:53:26 -0300

    FIX: Implementa o popup de redefinição de senha

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.storage)
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
