787cc0458cbed5b9745f45962ea967066a36f347
Misael Marcelino <misael.marcelino@outlook.com>
2025-09-17 20:01:50 -0300

    FIX: Implementa a comunicacao com o endpoint de login do backend

---
+++ mobile/app/src/main/java/br/com/fiap/softmind/data/remote/ApiClient.kt
@@ -0,0 +1,27 @@
+package br.com.fiap.softmind.data.remote
+
+import br.com.fiap.softmind.data.remote.api.AuthService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private const val BASE_URL = "http://127.0.0.1:8000"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging).build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val authService: AuthService = retrofit.create(AuthService::class.java)
+
+}
