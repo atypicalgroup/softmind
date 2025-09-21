757f3e7befb4a593eb4c5d5bb7ff812f93f816ca
Misael Marcelino <misael.marcelino@outlook.com>
2025-09-21 03:59:31 -0300

    FEAT: cria o portal administrativo com eus acessos

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Reset } from './auth/reset/reset';
+import { Validate } from './auth/validate/validate';
+import { Painel } from './portal/layout/painel/painel';
+import { Dashboard } from './portal/pages/dashboard/dashboard';
+import { Profile } from './portal/pages/profile/profile';
+import { Reports } from './portal/pages/reports/reports';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'login', component: Login },
+  { path: 'reset', component: Reset},
+  { path: 'validate', component: Validate},
+  {
+    path: 'portal',
+    component: Painel,
+    children: [
+      { path: 'dashboard', component: Dashboard},
+      { path: 'relatorios', component: Reports},
+      { path: 'perfil', component: Profile},
+      { path: '', redirectTo:'dashboard', pathMatch: 'full'}
+    ]
+  },
+  { path: '**', redirectTo: ''}
+];
---
+++ web/src/app/auth/validate/validate.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validate',
+  imports: [],
+  templateUrl: './validate.html',
+  styleUrl: './validate.scss'
+})
+export class Validate {
+
+}
