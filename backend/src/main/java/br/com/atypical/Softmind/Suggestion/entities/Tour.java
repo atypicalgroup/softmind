c0cf27fd301addcc6237ce0f39299aba0ca7a986
Lejunn100 <lejunsak@gmail.com>
2025-09-22 21:09:48 -0300

    Sugestão passeio finalizada, necessita de testes para finalizar 100%

--- backend/src/main/java/br/com/atypical/Softmind/Suggestion/entities/Tour.java
+++ backend/src/main/java/br/com/atypical/Softmind/Suggestion/entities/Tour.java
@@ -9,7 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Tour {
 
-    private String tipoPasseio;
-    private String descricaoPasseio;
-    private String localPasseio;
+    private String nameTour;
+    private String typeTour;
+    private String descriptionTour;
+    private String placeTour;
 }
