eca837da0607695d381b7341769e2567a2ed74f5
Misael Marcelino <misael.marcelino@outlook.com>
2025-10-01 21:54:23 -0300

    REFACT: Corrigi os serviços de sugestoes

--- backend/src/main/java/br/com/atypical/Softmind/Suggestion/service/SuggestionService.java
+++ backend/src/main/java/br/com/atypical/Softmind/Suggestion/service/SuggestionService.java
@@ -2,27 +2,33 @@ package br.com.atypical.Softmind.Suggestion.service;
 
 import br.com.atypical.Softmind.Movie.dto.MovieDto;
 import br.com.atypical.Softmind.Movie.service.MovieService;
+import br.com.atypical.Softmind.PhysicalActivity.service.PhysicalActService;
+import br.com.atypical.Softmind.Support.service.SupportService;
 import br.com.atypical.Softmind.Survey.entities.Answer;
 import br.com.atypical.Softmind.Survey.entities.SurveyResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class SuggestionService {
 
     private final MovieService movieService;
+    private final SupportService supportService;
+    private final PhysicalActService physicalActService;
 
-    public List<MovieDto> suggestMovies(SurveyResponse response) {
-        // 1. Extrair sentimento da primeira resposta
-        String feeling = response.getAnswers().stream()
-                .findFirst()
-                .map(Answer::getResponse)
-                .orElseThrow(() -> new RuntimeException("Resposta da pergunta 1 não encontrada"));
+    public Map<String, Object> getSuggestionsByMood(String feeling) {
+        var movies = movieService.getMoviesByFeeling(feeling);
+        var supports = supportService.listAll();
+        var activities = physicalActService.buscarVideosAtividadeFisica();
 
-        // 2. Consultar filmes pelo sentimento
-        return movieService.getMoviesByFeeling(feeling);
+        return Map.of(
+                "movies", movies,
+                "supports", supports,
+                "physicalActivities", activities
+        );
     }
 }
