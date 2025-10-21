bb9442675d579ba84b2d863375a8280633c7b989
Misael Marcelino <misael.marcelino@outlook.com>
2025-10-20 22:22:20 -0300

    FEAT: Cria recuperar Senha

--- web/src/app/auth/reset-password/reset-password.spec.ts
+++ web/src/app/auth/reset-password/reset-password.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Reset } from './reset';
+import { ResetPassword } from './reset-password';
 
-describe('Reset', () => {
-  let component: Reset;
-  let fixture: ComponentFixture<Reset>;
+describe('ResetPassword', () => {
+  let component: ResetPassword;
+  let fixture: ComponentFixture<ResetPassword>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Reset]
+      imports: [ResetPassword]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Reset);
+    fixture = TestBed.createComponent(ResetPassword);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
